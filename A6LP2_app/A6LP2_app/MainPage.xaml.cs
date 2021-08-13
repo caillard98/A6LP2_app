95fd9627da1fceca9a2ea8db4d66950f00d57706
Caio Matos <caio.matos@olos.com.br>
2021-08-13 13:11:44 -0300

    First commit

---
+++ A6LP2_app/A6LP2_app/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace A6LP2_app
+{
+    public partial class MainPage : ContentPage
+    {
+        int servings = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateServingsControls();
+        }
+
+        private async void Servings_Minus(object sender, EventArgs e)
+        {
+            servings--;
+            UpdateServingsControls();
+        }
+
+        private async void Servings_Plus(object sender, EventArgs e)
+        {
+            servings++;
+            UpdateServingsControls();
+        }
+        private void UpdateServingsControls()
+        {
+            _Servings.Text = servings.ToString();
+            if (servings == 1)
+            {
+                btnServingsMinus.IsEnabled = false;
+            } 
+            else if (servings == 15)
+            {
+                btnServingsPlus.IsEnabled = false;
+            }
+            else if (!btnServingsMinus.IsEnabled || !btnServingsPlus.IsEnabled)
+            {
+                btnServingsMinus.IsEnabled = true;
+                btnServingsPlus.IsEnabled = true;
+            }
+        }
+    }
+}
